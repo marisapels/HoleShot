3854d5afdfede7c42ba2f89df9e01b3ea786872d
Māris Apels <maris@apelslab.com>
2018-02-13 12:40:59 +0200

    Layout in progress

---
+++ HoleShot/ClientApp/components/LayoutNavMenu/UserPanel.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+
+class UserPanel extends React.Component {
+    public render() {
+        return (
+            <div className="user-panel">
+                <div className="pull-left image">
+                    <img src="dist/img/user2-160x160.jpg" className="img-circle" alt="User Image"/>
+                </div>
+                <div className="pull-left info">
+                    <p>Alexander Pierce</p>
+                    <a href="#"><i className="fa fa-circle text-success"></i> Online</a>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default UserPanel
